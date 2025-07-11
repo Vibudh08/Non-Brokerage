746a3c22e637e29cccbbfea8fdd22962690ecfb3
Vibudh08 <vibudhrathore08@gmail.com>
2025-07-11 18:11:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header />
+      <Routes>
+      </Routes>
+    </BrowserRouter>
+  );
+}
